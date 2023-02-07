24358200a6ba0e52fd78a89ef9ddfa3ac08d4073
Zachary Bordelon <zackmbordelon@gmail.com>
2023-02-07 13:32:01 -0600

    setup react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+//console.log('Hello World')
+
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+const App = () =>{
+    return (
+        <div>
+            <header>
+                Hello World
+            </header>
+        </div>
+    )
+}
+
+
+
+
+const container = document.getElementById('app')
+const root = createRoot(container); 
+root.render(<App />)
