68670af1b82328bfc9a09acd3ded5ad914fc3de3
Zachary Bordelon <zackmbordelon@gmail.com>
2023-02-09 14:16:38 -0600

    added posts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-//console.log('Hello World')
 
 import { createRoot } from 'react-dom/client';
-import React from 'react';
-const App = () =>{
-    return (
-        <div>
-            <header>
-                Hello World
-            </header>
-        </div>
-    )
+import React, { useState, useEffect } from 'react';
+
+
+const App = () => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const getPosts = async() => {
+      const response = await fetch('https://strangers-things.herokuapp.com/api/2211-ftb-et-web-am/posts')
+      const data = await response.json()
+      setPosts(data.data.posts)
+    }
+    getPosts()
+  }, []);
+
+  return (
+    <div>
+      <ol>
+      {
+        posts.map((post) => { 
+          console.log(post)
+          return <li>{post.title} - {post.location} - {post.author.username}</li>
+
+        })
+         
+      }
+      </ol>
+    </div>
+  )
 }
 
 
@@ -18,3 +36,4 @@ const App = () =>{
 const container = document.getElementById('app')
 const root = createRoot(container); 
 root.render(<App />)
+
