fad1ab5201726bd485b9ca135501f0dbd6e6ab0c
Zachary Bordelon <zackmbordelon@gmail.com>
2023-02-15 13:03:22 -0600

    login/regi display

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LogIn = () => { 
+    const [ displayLogInButton, setDisplayLogInButton ] = useState(true)
+
+    const displayRegisterButton = () =>{
+      setDisplayLogInButton(false);
+    }
+
     return(
+      <div>
         <h1>Log In</h1>
+        <form>
+          <input placeholder='Username'></input>
+          <input placeholder='Password'></input>
+          {
+            displayLogInButton ? 
+            <>
+              <button>Log In</button>
+              <button onClick={displayRegisterButton}>Register Here</button>
+            </> : 
+            <>
+              <input placeholder="confirm password"></input>          
+              <button>Register</button>
+              <button onClick={()=> setDisplayLogInButton(true)}>Return to Log In</button>
+            </>
+          }
+        </form>
+      </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ const App = () => {
       <Routes>
         <Route path='/login' element={<LogIn />} />
         <Route path='/' element={<ItemPosts posts={posts}/>} />
+        
       </Routes>
     </div>
   )
