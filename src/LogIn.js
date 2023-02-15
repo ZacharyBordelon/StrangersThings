3a50af6f558277b1ecac67b12e9b5e0a4edb3d30
Zachary Bordelon <zackmbordelon@gmail.com>
2023-02-15 13:59:55 -0600

    onChange

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 
 const LogIn = () => { 
-    const [ displayLogInButton, setDisplayLogInButton ] = useState(true)
+    const [ displayLogInButton, setDisplayLogInButton] = useState(true)
+    const [ usernameInput, setUsernameInput] = useState('')
+    const [ passwordInput, setPasswordInput] = useState('')
 
     const displayRegisterButton = () =>{
       setDisplayLogInButton(false);
+    }
+    const logIn = () => {
+      
     }
 
     return(
       <div>
         <h1>Log In</h1>
-        <form>
-          <input placeholder='Username'></input>
-          <input placeholder='Password'></input>
+        <form onSubmit={logIn}>
+          <input placeholder='Username' onChange={(event) => setUsernameInput(event.target.value)}></input>
+          <input placeholder='Password' onChange={(event) => setPasswordInput(event.target.value)}></input>
           {
             displayLogInButton ? 
             <>
@@ -20,9 +25,9 @@ const LogIn = () => {
               <button onClick={displayRegisterButton}>Register Here</button>
             </> : 
             <>
-              <input placeholder="confirm password"></input>          
+              {/* <input placeholder="confirm password"></input>           */}
               <button>Register</button>
-              <button onClick={()=> setDisplayLogInButton(true)}>Return to Log In</button>
+              <button onClick={() => setDisplayLogInButton(true)}>Return to Log In</button>
             </>
           }
         </form>
